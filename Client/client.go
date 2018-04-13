a0b8c72b7fac4bfaa7808d0f30a22a7d4790c04e
suraj-singh-cpi <surajrider@gmail.com>
2018-04-14 00:37:13 +0530

    Approach for version 1 client chat app

--- Client/client.go
+++ Client/client.go
@@ -8,26 +8,6 @@ import (
 )
 
 func main() {
-	// Connect to local host socket
-	connect, err := net.Dial("tcp", "127.0.0.1:9090")
-	if err != nil {
-		fmt.Println(err)
-	}
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter text to send!")
-	//writer := bufio.NewWriter(connect)
-	for {
-		text, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading input from console")
-		}
-		//_, err = writer.WriteString(text)
-		// Send to server socket
-		fmt.Fprintf(connect, text)
-		if err != nil {
-			fmt.Println(err)
-		}
-		serverMessage, _ := bufio.NewReader(connect).ReadString('\n')
-		fmt.Println(serverMessage)
-	}
+	// TODO ... This should be client requesting a bi-directional websocket connection
+	// Future ... Client should request to connect to other existing client
 }
--- Server/server.go
+++ Server/server.go
@@ -6,42 +6,15 @@ import (
 	"net"
 	_ "net"
 	"strings"
+	"flag"
+
+	"github.com/gorilla/websocket"
 )
 
+var url = flag.String("addr", "localhost:8080", "http service address")
 func main() {
-	fmt.Println("This is direct beginning of go program")
-	listners, err := net.Listen("tcp", ":9090")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer func() {
-		listners.Close()
-		fmt.Println("Closing listning to new connections")
-	}() // implicit call
-
-	for {
-		conn, err := listners.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection", err)
-		}
-		go handleNewConnection(conn)
-	}
-}
-
-func handleNewConnection(connection net.Conn) {
-	fmt.Println("Handling new connection request")
-	defer connection.Close()
-	bufRead := bufio.NewReader(connection)
-	for {
-		data, err := bufRead.ReadString('\n')
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(data)
-		fmt.Fprintf(connection, strings.ToUpper(data))
-		//_, err = bufio.NewWriter(connection).WriteString(strings.ToUpper(data))
-		if err != nil {
-			fmt.Println("Error writing back to the client")
-		}
-	}
+	// TODO ... Server should handle new connections and persist the same via websocket connections
+	// Anything typed here should be shown to clients attached to this server. 
+	// Future ... Multi-chat rooms, giving client option to connect with other clients. Peer to peer chat & group chats
+	// Server will just act as a hub connecting different clients.
 }
